f1642758e92eb7621abfa431338794bda1b42993
Banjeoin <e.odrisceoil@gmail.com>
2026-08-06 14:09:41 +0100

    DRLavaControl: cleanup, new style update

--- src/dlls/objects/737_DRLavaControl/DRlavacontrol.c
+++ src/dlls/objects/737_DRLavaControl/DRlavacontrol.c
@@ -6,12 +6,13 @@
 #include "game/gamebits.h"
 #include "game/objects/object.h"
 #include "game/objects/object_id.h"
+#include "macros.h"
+#include "sys/gfx/model.h"
 #include "sys/main.h"
 #include "sys/objects.h"
-#include "sys/gfx/model.h"
 #include "sys/objtype.h"
-#include "types.h"
 #include "sys/rand.h"
+#include "types.h"
 
 typedef struct {
 /*0*/ Object* lfxEmitter;
@@ -31,23 +32,27 @@ typedef struct {
 /*1E*/    s16 gameBitFrozen;    //gamebit to set when fully cooled with Ice Blast
 } DRLavaControl_Setup;
 
+typedef enum {
+    DRLavaControl_FLAG_1_Frozen = 1
+} DRLavaControl_Flags;
+
 /*0x0*/ static s16 lfxEmitterUnk1E[] = {
     547, 550, 553
 };
 
 static void DRLavaControl_freeze(Object* self);
-static void DRLavaControl_freeze_update_effects(Object* self, DRLavaControl_Data* objData, s32 effectIndex);
-static void DRLavaControl_create_particles(Object* self, u32 effectIndex);
-static Object* DRLavaControl_create_light(Object* self, s32 lfxSetupUnk1E);
+static void DRLavaControl_freezeUpdateEffects(Object* self, DRLavaControl_Data* objData, s32 effectIndex);
+static void DRLavaControl_createParticles(Object* self, u32 effectIndex);
+static Object* DRLavaControl_createLight(Object* self, s32 lfxSetupUnk1E);
 
 // offset: 0x0 | ctor
-void DRLavaControl_ctor(void *dll) { }
+void DRLavaControl_ctor(void* dll) { }
 
 // offset: 0xC | dtor
-void DRLavaControl_dtor(void *dll) { }
+void DRLavaControl_dtor(void* dll) { }
 
 // offset: 0x18 | func: 0 | export: 0
-void DRLavaControl_setup(Object* self, DRLavaControl_Setup* objSetup, s32 arg2) {
+void DRLavaControl_obj_Setup(Object* self, DRLavaControl_Setup* objSetup, s32 reset) {
     DRLavaControl_Data* objData;
     s32 isFrozen;
 
@@ -56,29 +61,31 @@ void DRLavaControl_setup(Object* self, DRLavaControl_Setup* objSetup, s32 arg2) 
     objData->freezeDuration = objData->freezeTimer;
     
     if (mainGetBits(objSetup->gameBitFrozen)) {
-        isFrozen = TRUE;
+        isFrozen = DRLavaControl_FLAG_1_Frozen;
     } else {
-        isFrozen = FALSE;
+        isFrozen = 0;
     }
     
     objData->flags |= isFrozen;
     objData->lfxEmitterVal = lfxEmitterUnk1E[objSetup->dataIndex];
-    if (objData->flags & 1) {
-        DRLavaControl_freeze_update_effects(self, objData, 0);
+
+    if (objData->flags & DRLavaControl_FLAG_1_Frozen) {
+        DRLavaControl_freezeUpdateEffects(self, objData, 0);
         return;
     }
-    DRLavaControl_freeze_update_effects(self, objData, 3);
+
+    DRLavaControl_freezeUpdateEffects(self, objData, 3);
     gDLL_5_AMSEQ2->vtbl->set(self, 0x102, 0, 0, 0);
 }
 
 // offset: 0x130 | func: 1 | export: 1
-void DRLavaControl_control(Object* self) {
+void DRLavaControl_obj_Control(Object* self) {
     DRLavaControl_Data* objData = self->data;
-    DRLavaControl_create_particles(self, objData->effectIndex);
+    DRLavaControl_createParticles(self, objData->effectIndex);
 }
 
 // offset: 0x170 | func: 2 | export: 2
-void DRLavaControl_update(Object* self) {
+void DRLavaControl_obj_Update(Object* self) {
     //Check if hit with Ice Blast Spell
     if (func_80025F40(self, NULL, NULL, NULL) == Damage_Type_Ice_Blast) {
         DRLavaControl_freeze(self);
@@ -86,25 +93,26 @@ void DRLavaControl_update(Object* self) {
 }
 
 // offset: 0x1D8 | func: 3 | export: 3
-void DRLavaControl_print(Object *self, Gfx **gdl, Mtx **mtxs, Vertex **vtxs, Triangle **pols, s8 visibility) { }
+void DRLavaControl_obj_Print(Object* self, Gfx** gdl, Mtx** mtxs, Vertex** vtxs, Triangle** pols, s8 visibility) { }
 
 // offset: 0x1F0 | func: 4 | export: 4
-void DRLavaControl_free(Object* self, s32 arg1) {
+void DRLavaControl_obj_Free(Object* self, s32 onlySelf) {
     DRLavaControl_Data* objData = self->data;
 
-    if (objData->lfxEmitter && (arg1 == 0)) {
+    if (objData->lfxEmitter && (onlySelf == FALSE)) {
         objFreeObject(objData->lfxEmitter);
     }
+
     gDLL_13_Expgfx->vtbl->func4(self);
 }
 
 // offset: 0x270 | func: 5 | export: 5
-u32 DRLavaControl_get_model_flags(Object *self) {
+u32 DRLavaControl_obj_GetModelFlags(Object* self) {
     return MODFLAGS_NONE;
 }
 
 // offset: 0x280 | func: 6 | export: 6
-u32 DRLavaControl_get_data_size(Object *self, u32 a1) {
+u32 DRLavaControl_obj_GetDataSize(Object* self, u32 offsetAddr) {
     return sizeof(DRLavaControl_Data);
 }
 
@@ -115,7 +123,7 @@ void DRLavaControl_freeze(Object* self) {
     DRLavaControl_Setup* objSetup;
 
     objData = self->data;
-    if (objData->flags & 1) {
+    if (objData->flags & DRLavaControl_FLAG_1_Frozen) {
         return;
     }
 
@@ -129,22 +137,22 @@ void DRLavaControl_freeze(Object* self) {
 
         if (objData->freezeTimer == 0) {
             dll_amSfx->Play(self, SOUND_80B_Crackling_Freezing, MAX_VOLUME, NULL, NULL, 0, NULL);
-            objData->flags |= 1;
+            objData->flags |= DRLavaControl_FLAG_1_Frozen;
             mainSetBits(objSetup->gameBitFrozen, TRUE);
-            // diPrintf(" bit set %i ", objSetup->gameBitFrozen);
+            STUBBED_PRINTF(" bit set %i ", objSetup->gameBitFrozen);
             mainIncrementBits(BIT_DR_Lava_Pools_Cooled_Count);
             gDLL_5_AMSEQ2->vtbl->set(self, 0x102, 0, 0, 0);
         }
     }
 
-    DRLavaControl_freeze_update_effects(self, objData, objData->freezeTimer / 5);
+    DRLavaControl_freezeUpdateEffects(self, objData, objData->freezeTimer / 5);
 }
 
 // offset: 0x47C | func: 8
-void DRLavaControl_freeze_update_effects(Object* self, DRLavaControl_Data* objData, s32 effectIndex) {
+void DRLavaControl_freezeUpdateEffects(Object* self, DRLavaControl_Data* objData, s32 effectIndex) {
     f32 distance;
     f32 temperature_tValue; //0.0 when cooled, 1.0 when hot
-    u16 dataUnused[] = { 288, 287, 286, 282 }; //unused indices?
+    u16 dataUnused[] = { 288, 287, 286, 282 }; //unused fxScales?
     Object* waveAnimator;
     
     if (objData->freezeTimer == 0) {
@@ -156,7 +164,8 @@ void DRLavaControl_freeze_update_effects(Object* self, DRLavaControl_Data* objDa
     distance = 500.0f;
     waveAnimator = objGetNearestTypeTo(OBJTYPE_WaveAnimator, self, &distance);
     if (waveAnimator) {
-        // diPrintf(" WAVE OBJ %x ", waveAnimator);
+        STUBBED_PRINTF(" WAVE OBJ %x ", waveAnimator->setup->uID);
+        //TODO: use WaveAnimator interface
         ((DLL_Unknown*)waveAnimator->dll)->vtbl->func[7].withOneS32OneF32((s32)waveAnimator, temperature_tValue); //blending wave amplitude to 0 as lava cools?
         ((DLL_Unknown*)waveAnimator->dll)->vtbl->func[8].withTwoArgs((s32)waveAnimator, 1);
         ((DLL_Unknown*)waveAnimator->dll)->vtbl->func[9].withFourArgs((s32)waveAnimator, 0xB3, 0x18, 0x18); //reddish RGB colour?
@@ -167,15 +176,15 @@ void DRLavaControl_freeze_update_effects(Object* self, DRLavaControl_Data* objDa
             objFreeObject(objData->lfxEmitter);
         }
         if (effectIndex) {
-            // diPrintf(" Creating LIGHT %i  x %f z %f \n", arg2, self->srt.transl.x, self->srt.transl.z);
-            objData->lfxEmitter = DRLavaControl_create_light(self, (objData->lfxEmitterVal - effectIndex) + 3);
+            STUBBED_PRINTF(" Creating LIGHT %i  x %f z %f \n", effectIndex, self->srt.transl.x, self->srt.transl.z);
+            objData->lfxEmitter = DRLavaControl_createLight(self, (objData->lfxEmitterVal - effectIndex) + 3);
         }
         objData->effectIndex = effectIndex;
     }
 }
 
 // offset: 0x604 | func: 9
-Object* DRLavaControl_create_light(Object* self, s32 lfxSetupUnk1E) {
+Object* DRLavaControl_createLight(Object* self, s32 lfxSetupUnk1E) {
     LFXEmitter_Setup* setup;
 
     setup = (LFXEmitter_Setup*)objAllocSetup(sizeof(LFXEmitter_Setup), OBJ_LFXEmitter);
@@ -202,7 +211,7 @@ Object* DRLavaControl_create_light(Object* self, s32 lfxSetupUnk1E) {
 }
 
 // offset: 0x720 | func: 10
-void DRLavaControl_create_particles(Object* self, u32 effectIndex) {
+void DRLavaControl_createParticles(Object* self, u32 effectIndex) {
     //Probabilities for particles to be created (e.g. 1/30, 1/5, 1/60)
     s32 particleChanceA[3] = {30, 5, 5};
     s32 particleChanceB[3] = {60, 10, 10};
@@ -217,7 +226,3 @@ void DRLavaControl_create_particles(Object* self, u32 effectIndex) {
         }
     }
 }
-
-/*0x0*/ static const char str_0[] = " bit set %i ";
-/*0x10*/ static const char str_10[] = " WAVE OBJ %x ";
-/*0x20*/ static const char str_20[] = " Creating LIGHT %i  x %f z %f \n";
--- src/dlls/objects/737_DRLavaControl/exports.s
+++ src/dlls/objects/737_DRLavaControl/exports.s
@@ -8,10 +8,10 @@ _exports:
 .dword DRLavaControl_dtor
 
 # export table
-/*0*/ .dword DRLavaControl_setup
-/*1*/ .dword DRLavaControl_control
-/*2*/ .dword DRLavaControl_update
-/*3*/ .dword DRLavaControl_print
-/*4*/ .dword DRLavaControl_free
-/*5*/ .dword DRLavaControl_get_model_flags
-/*6*/ .dword DRLavaControl_get_data_size
+/*0*/ .dword DRLavaControl_obj_Setup
+/*1*/ .dword DRLavaControl_obj_Control
+/*2*/ .dword DRLavaControl_obj_Update
+/*3*/ .dword DRLavaControl_obj_Print
+/*4*/ .dword DRLavaControl_obj_Free
+/*5*/ .dword DRLavaControl_obj_GetModelFlags
+/*6*/ .dword DRLavaControl_obj_GetDataSize
