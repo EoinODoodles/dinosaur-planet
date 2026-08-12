0bbf1933c8eb40b8a91a93a551a5357dc7077a9c
Banjeoin <e.odrisceoil@gmail.com>
2026-08-12 18:26:12 +0100

    WarpPoint: docs, new style update

--- src/dlls/objects/275_WarpPoint/WarpPoint.c
+++ src/dlls/objects/275_WarpPoint/WarpPoint.c
@@ -1,16 +1,16 @@
 #include "dll.h"
+#include "sys/gfx/model.h"
 #include "sys/main.h"
 #include "sys/map.h"
 #include "sys/objects.h"
 #include "game/objects/object.h"
-#include "sys/gfx/model.h"
 
 typedef struct {
-/*00*/ s16 counter;
+/*00*/ s16 warpDelayTimer;
 /*02*/ s16 gamebit;
 /*04*/ s16 objectSeqIndex;
-/*08*/ f32 dist;
-/*0C*/ u8 unkC;
+/*08*/ f32 range;
+/*0C*/ u8 objSeqPlayed;
 } WarpPoint_Data;
 
 typedef struct {
@@ -19,54 +19,69 @@ typedef struct {
 /*19*/ u8 unk19;
 /*1A*/ s8 warpID;
 /*1B*/ s8 objectSeqIndex;
-/*1C*/ s8 unk1C;
-/*1D*/ s8 unk1D;
-/*1E*/ s8 dist;
+/*1C*/ s8 isInboundWarp;
+/*1D*/ s8 mode;
+/*1E*/ s8 quarterRange;
 /*20*/ s16 gamebit;
 } WarpPoint_Setup;
 
-enum Unk1D {
-    UNK1D_0,
-    UNK1D_1,
-    UNK1D_2,
-    UNK1D_3,
-    UNK1D_4
-};
+typedef enum {
+    WarpPoint_OBJSEQ_0, //0x81 (outbound) (FADE NOT CHECKED) Warp immediately via objSeq message, has unseen player anims too (curling into ball?)
+    WarpPoint_OBJSEQ_1, //0x82 (inbound) (no fade) Player curled and rolling in midair, uncurls and lands
+    WarpPoint_OBJSEQ_2, //0x15E (inbound) (FADE NOT CHECKED) Player spat out during Galadon battle #1
+    WarpPoint_OBJSEQ_3, //0x196 (inbound) (FADE NOT CHECKED) Player landing in Galadon's stomach
+    WarpPoint_OBJSEQ_4, //0x15F (inbound) (FADE NOT CHECKED) Player spat out during Galadon battle #2
+    WarpPoint_OBJSEQ_5, //0x1C3 (inbound) (FADE NOT CHECKED) Rocky swapping to Sabre (unused early draft?)
+    WarpPoint_OBJSEQ_6, //0x2CE (inbound) (fade in) Sabre and Tricky thrown out of DarkIce Mines
+    WarpPoint_OBJSEQ_7, //0x382 (inbound) (no fade) Sabre falling through vertical chute and crashing to ground in Lower Dragon Rock
+    WarpPoint_OBJSEQ_8, //0x429 (inbound) (fade in) Sabre and Tricky walking towards camera 
+    WarpPoint_OBJSEQ_9  //0x447 (inbound) (fade in) Player walks in towards camera, camera cuts behind them
+} WarpPoint_ObjSeqs;
 
-static int WarpPoint_anim_callback(Object *self, Object *animObj, AnimObj_Data *animObjData, s8 arg3);
+typedef enum {
+    WarpPoint_MODE_0, //Play an arrival objSeq (fade in); warp when nearby (fade out) (ignores delay timer)
+    WarpPoint_MODE_1, //Play arrival objSeq1 (uncurling from ball) (fade in); warp via objSeq0 (no fade out) (use delay timer)
+    WarpPoint_MODE_2, //Play an arrival objSeq if a gamebit it set (no fade in); warp and unset gamebit if arrival objSeq played (no fade out) (checks delay timer, but ignores it in practice by setting it to 0)
+    WarpPoint_MODE_3, //(Inbound only) Play an arrival objSeq if a gamebit is set (no fade in) and unset the gamebit; no warp away
+    WarpPoint_MODE_4  //Play an arrival objSeq if a warpID is specified (fade in); warp if a gamebit is set, unsetting gamebit (fade out) (uses delay timer)
+} WarpPoint_Modes;
+
+static int WarpPoint_animCallback(Object* self, Object* animObj, AnimObj_Data* animObjData, s8 prevCallbackValue);
 
 // offset: 0x0 | ctor
-void WarpPoint_ctor(void *dll) { }
+void WarpPoint_ctor(void* dll) { }
 
 // offset: 0xC | dtor
-void WarpPoint_dtor(void *dll) { }
+void WarpPoint_dtor(void* dll) { }
 
 // offset: 0x18 | func: 0 | export: 0
-void WarpPoint_setup(Object *self, WarpPoint_Setup *setup, s32 arg2) {
-    WarpPoint_Data *objdata;
+void WarpPoint_obj_Setup(Object* self, WarpPoint_Setup* setup, s32 reset) {
+    WarpPoint_Data* objdata = self->data;
 
-    objdata = self->data;
-    self->animCallback = WarpPoint_anim_callback;
+    self->animCallback = WarpPoint_animCallback;
     self->srt.yaw = setup->yaw << 8;
-    objdata->counter = 30;
-    objdata->dist = setup->dist * 4;
+
+    objdata->warpDelayTimer = 30;
+    objdata->range = setup->quarterRange * 4;
     objdata->gamebit = setup->gamebit;
     objdata->objectSeqIndex = setup->objectSeqIndex;
-    if (setup->unk1C != 0) {
-        objdata->unkC = 0;
+
+    if (setup->isInboundWarp) {
+        objdata->objSeqPlayed = FALSE;
     } else {
-        objdata->unkC = 1;
+        objdata->objSeqPlayed = TRUE;
     }
-    if (setup->unk1D == UNK1D_2) {
-        objdata->counter = 0;
+
+    if (setup->mode == WarpPoint_MODE_2) {
+        objdata->warpDelayTimer = 0;
     }
 }
 
 // offset: 0xAC | func: 1 | export: 1
-void WarpPoint_control(Object *self) {
-    Object *player;
-    WarpPoint_Setup *setup;
-    WarpPoint_Data *objdata;
+void WarpPoint_obj_Control(Object* self) {
+    Object* player;
+    WarpPoint_Setup* setup;
+    WarpPoint_Data* objdata;
     f32 dx;
     f32 dy;
     f32 dz;
@@ -75,88 +90,110 @@ void WarpPoint_control(Object *self) {
     setup = (WarpPoint_Setup*)self->setup;
     objdata = self->data;
     player = objGetPlayer();
-    objdata->counter -= gUpdateRate;
-    if (objdata->counter < 0) {
-        objdata->counter = 0;
+
+    //Don't allow warping until a timer has run out (for modes 1, 4)
+    objdata->warpDelayTimer -= gUpdateRate;
+    if (objdata->warpDelayTimer < 0) {
+        objdata->warpDelayTimer = 0;
     }
-    switch (setup->unk1D) {
-    case UNK1D_0:
-        if (D_800B4A5E >= 0) {
+
+    switch (setup->mode) {
+    case WarpPoint_MODE_0:
+        //If this is an inbound Warp Point, play the specified arrival objSeq when the player arrives nearby (variable range)
+        if (D_800B4A5E > NO_WARP_ID) {
             dx = player->srt.transl.x - self->srt.transl.x;
             dy = player->srt.transl.y - self->srt.transl.y;
             dz = player->srt.transl.z - self->srt.transl.z;
             dist = sqrtf(SQ(dx) + SQ(dy) + SQ(dz));
-            if ((objdata->unkC == 0) && (setup->unk1C != 0) && (dist < objdata->dist) && (player->parent == self->parent)) {
+            if ((objdata->objSeqPlayed == FALSE) && setup->isInboundWarp && (dist < objdata->range) && (player->parent == self->parent)) {
                 gDLL_3_Animation->vtbl->start_obj_sequence(objdata->objectSeqIndex, self, -1);
-                D_80092A78 = 2;
-                objdata->unkC = 1;
+                D_80092A78 = 2; //Fade in from black
+                objdata->objSeqPlayed = TRUE;
             }
         }
-        if ((setup->warpID >= 0) && (vec3Distance(&self->globalPosition, &player->globalPosition) < objdata->dist)) {
+
+        //If a warpID is provided, fade to black and warp the player when they're nearby (variable range)
+        if ((setup->warpID > NO_WARP_ID) && (vec3Distance(&self->globalPosition, &player->globalPosition) < objdata->range)) {
             mapWarpPlayer(setup->warpID, TRUE);
         }
         break;
-    case UNK1D_1:
+    case WarpPoint_MODE_1:
         dx = player->srt.transl.x - self->srt.transl.x;
         dy = player->srt.transl.y - self->srt.transl.y;
         dz = player->srt.transl.z - self->srt.transl.z;
         dist = sqrtf(SQ(dx) + SQ(dy) + SQ(dz));
-        if ((D_800B4A5E >= 0) && (setup->unk1C != 0) && (dist < 100.0f) && (player->parent == self->parent)) {
-            gDLL_3_Animation->vtbl->start_obj_sequence(1, self, -1);
-            D_80092A78 = 2;
+
+        //If this is an inbound Warp Point, play objSeq1 (player uncurling from ball) when the player arrives nearby (fixed range of 100)
+        if ((D_800B4A5E > NO_WARP_ID) && setup->isInboundWarp && (dist < 100.0f) && (player->parent == self->parent)) {
+            gDLL_3_Animation->vtbl->start_obj_sequence(WarpPoint_OBJSEQ_1, self, -1);
+            D_80092A78 = 2; //Fade in from black
         }
-        if ((objdata->counter == 0) && (dist < setup->dist)) {
-            if ((setup->warpID >= 0) && (setup->warpID >= 0)) {
-                gDLL_3_Animation->vtbl->start_obj_sequence(0, self, -1);
+
+        //If a warpID is provided, play objSeq0 and warp via its anim message when the player is nearby (variable range/4) (waits for timer to expire)
+        if ((objdata->warpDelayTimer == 0) && (dist < setup->quarterRange)) {
+            if ((setup->warpID > NO_WARP_ID) && (setup->warpID > NO_WARP_ID)) { //@bug: accidentally pasted condition twice?
+                gDLL_3_Animation->vtbl->start_obj_sequence(WarpPoint_OBJSEQ_0, self, -1);
             }
         }
         break;
-    case UNK1D_2:
-        if (objdata->dist != 0.0f) {
+    case WarpPoint_MODE_2:
+        if (objdata->range != 0.0f) {
             dx = player->globalPosition.x - self->globalPosition.x;
             dy = player->globalPosition.y - self->globalPosition.y;
             dz = player->globalPosition.z - self->globalPosition.z;
             dist = sqrtf(SQ(dx) + SQ(dy) + SQ(dz));
         } else {
-            dist = objdata->dist;
+            dist = objdata->range;
         }
-        if ((mainGetBits(objdata->gamebit)) && (objdata->unkC == 0) && (setup->unk1C != 0) && (dist <= objdata->dist) && (player->parent == self->parent)) {
+
+        //If this is an inbound Warp Point, play the specified arrival objSeq when the player's nearby and a specified gamebit is set (variable range)
+        if (mainGetBits(objdata->gamebit) && (objdata->objSeqPlayed == FALSE) && setup->isInboundWarp && (dist <= objdata->range) && (player->parent == self->parent)) {
             gDLL_3_Animation->vtbl->start_obj_sequence(objdata->objectSeqIndex, self, -1);
-            objdata->unkC = 1;
-        } else if (objdata->unkC == 1) {
-            if ((mainGetBits(objdata->gamebit)) && (objdata->counter == 0) && (dist <= objdata->dist) && (setup->warpID >= 0)) {
-                mainSetBits(objdata->gamebit, 0);
+            objdata->objSeqPlayed = TRUE;
+        } else if (objdata->objSeqPlayed == TRUE) {
+        //If a warpID is provided and the arrival sequence played, warp the player immediately (without fade) when they're nearby and unset the gamebit (variable range)
+        //(Waits for timer to expire, but obj_Setup has the timer expire immediately when using this mode!)
+            if ((mainGetBits(objdata->gamebit)) && (objdata->warpDelayTimer == 0) && (dist <= objdata->range) && (setup->warpID > NO_WARP_ID)) {
+                mainSetBits(objdata->gamebit, FALSE);
                 mapWarpPlayer(setup->warpID, FALSE);
             }
         }
         break;
-    case UNK1D_3:
+    case WarpPoint_MODE_3:
         dx = player->srt.transl.x - self->srt.transl.x;
         dy = player->srt.transl.y - self->srt.transl.y;
         dz = player->srt.transl.z - self->srt.transl.z;
         dist = sqrtf(SQ(dx) + SQ(dy) + SQ(dz));
-        if ((mainGetBits(objdata->gamebit)) && (objdata->unkC == 0) && (setup->unk1C != 0) && (dist < objdata->dist) && (player->parent == self->parent)) {
-            mainSetBits(objdata->gamebit, 0);
+
+        //If this is an inbound Warp Point, play the specified arrival objSeq when the player's nearby and a specified gamebit is set (variable range)
+        if (mainGetBits(objdata->gamebit) && (objdata->objSeqPlayed == FALSE) && setup->isInboundWarp && (dist < objdata->range) && (player->parent == self->parent)) {
+            mainSetBits(objdata->gamebit, FALSE);
             gDLL_3_Animation->vtbl->start_obj_sequence(objdata->objectSeqIndex, self, -1);
-            objdata->unkC = 1;
+            objdata->objSeqPlayed = TRUE;
         }
+
+        //NOTE: no outbound warp in this mode (unless objectSeqIndex is 0, activating the animCallback function's warp)
         break;
-    case UNK1D_4:
-        if (objdata->dist != 0.0f) {
+    case WarpPoint_MODE_4:
+        if (objdata->range != 0.0f) {
             dx = player->globalPosition.x - self->globalPosition.x;
             dy = player->globalPosition.y - self->globalPosition.y;
             dz = player->globalPosition.z - self->globalPosition.z;
             dist = sqrtf(SQ(dx) + SQ(dy) + SQ(dz));
         } else {
-            dist = objdata->dist;
+            dist = objdata->range;
         }
-        if ((D_800B4A5E >= 0) && (objdata->unkC == 0) && (setup->unk1C != 0) && (dist < objdata->dist) && (player->parent == self->parent)) {
+
+        //If this is an inbound Warp Point, play the specified arrival objSeq when the player arrives nearby (variable range)
+        if ((D_800B4A5E > NO_WARP_ID) && (objdata->objSeqPlayed == FALSE) && setup->isInboundWarp && (dist < objdata->range) && (player->parent == self->parent)) {
             gDLL_3_Animation->vtbl->start_obj_sequence(objdata->objectSeqIndex, self, -1);
-            D_80092A78 = 2;
-            objdata->unkC = 1;
+            D_80092A78 = 2; //Fade in from black
+            objdata->objSeqPlayed = TRUE;
         }
-        if ((mainGetBits(objdata->gamebit)) && (objdata->counter == 0) && (dist <= objdata->dist) && (setup->warpID >= 0)) {
-            mainSetBits(objdata->gamebit, 0);
+
+        //If a a specified gamebit is set and the timer's expired and warpID is provided, unset the gamebit and fade out and warp the player when they're nearby (variable range)
+        if (mainGetBits(objdata->gamebit) && (objdata->warpDelayTimer == 0) && (dist <= objdata->range) && (setup->warpID > NO_WARP_ID)) {
+            mainSetBits(objdata->gamebit, FALSE);
             mapWarpPlayer(setup->warpID, TRUE);
         }
         break;
@@ -164,39 +201,40 @@ void WarpPoint_control(Object *self) {
 }
 
 // offset: 0x83C | func: 2 | export: 2
-void WarpPoint_update(Object *self) { }
+void WarpPoint_obj_Update(Object* self) { }
 
 // offset: 0x848 | func: 3 | export: 3
-void WarpPoint_print(Object *self, Gfx **gdl, Mtx **mtxs, Vertex **vtxs, Triangle **pols, s8 visibility) {
+void WarpPoint_obj_Print(Object* self, Gfx** gdl, Mtx** mtxs, Vertex** vtxs, Triangle** pols, s8 visibility) {
     if (visibility) {
         if (self) { }
     }
 }
 
 // offset: 0x868 | func: 4 | export: 4
-void WarpPoint_free(Object *self, s32 a1) { }
+void WarpPoint_obj_Free(Object* self, s32 onlySelf) { }
 
 // offset: 0x878 | func: 5 | export: 5
-u32 WarpPoint_get_model_flags(Object *self) {
+u32 WarpPoint_obj_GetModelFlags(Object* self) {
     return MODFLAGS_1;
 }
 
 // offset: 0x888 | func: 6 | export: 6
-u32 WarpPoint_get_data_size(Object *self, u32 a1) {
+u32 WarpPoint_obj_GetDataSize(Object* self, u32 offsetAddr) {
     return sizeof(WarpPoint_Data);
 }
 
 // offset: 0x89C | func: 7
-int WarpPoint_anim_callback(Object *self, Object *animObj, AnimObj_Data *animObjData, s8 arg3) {
-    WarpPoint_Setup *setup;
+int WarpPoint_animCallback(Object* self, Object* animObj, AnimObj_Data* animObjData, s8 prevCallbackValue) {
+    WarpPoint_Setup* setup = (WarpPoint_Setup*)self->setup;
 
-    setup = (WarpPoint_Setup*)self->setup;
-    if ((setup->unk1D != UNK1D_2) && (animObjData->lastMessage == 1)) {
+    //Warp the player (with a fade-to-black) via animMessage1
+    if ((setup->mode != WarpPoint_MODE_2) && (animObjData->lastMessage == 1)) {
         if (setup->warpID >= 0) {
-            mapWarpPlayer(setup->warpID, 1);
+            mapWarpPlayer(setup->warpID, TRUE);
             animObjData->lastMessage = 0;
         }
     }
+
     return 0;
 }
 
--- src/dlls/objects/275_WarpPoint/exports.s
+++ src/dlls/objects/275_WarpPoint/exports.s
@@ -8,10 +8,10 @@ _exports:
 .dword WarpPoint_dtor
 
 # export table
-/*0*/ .dword WarpPoint_setup
-/*1*/ .dword WarpPoint_control
-/*2*/ .dword WarpPoint_update
-/*3*/ .dword WarpPoint_print
-/*4*/ .dword WarpPoint_free
-/*5*/ .dword WarpPoint_get_model_flags
-/*6*/ .dword WarpPoint_get_data_size
+/*0*/ .dword WarpPoint_obj_Setup
+/*1*/ .dword WarpPoint_obj_Control
+/*2*/ .dword WarpPoint_obj_Update
+/*3*/ .dword WarpPoint_obj_Print
+/*4*/ .dword WarpPoint_obj_Free
+/*5*/ .dword WarpPoint_obj_GetModelFlags
+/*6*/ .dword WarpPoint_obj_GetDataSize
